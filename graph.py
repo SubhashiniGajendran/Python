db063bcd8bca8b5b16e5ab36b83885c6c82446b5
SubhashiniGajendran <subhashini.gajendran@gmail.com>
2025-11-12 21:48:28 +0530

    first commit

---
+++ graph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y1 = [1, 2, 3, 4, 5]
+y2 = [2, 4, 6, 8, 10]
+
+plt.plot(x, y1, label='Line 1', color='red')
+plt.plot(x, y2, label='Line 2', color='blue')
+
+plt.title("Multiple Line Graph")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.legend()
+plt.show()
